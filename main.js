ee6536edfc1fd01394772c3553cd68320d12037c
Virat Surya44 <102590571+Virat-Surya4390@users.noreply.github.com>
2022-12-14 16:14:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
